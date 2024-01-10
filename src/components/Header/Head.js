72906369cacb46f101f8e5add42e9d0056ccd977
veeran <veeran@kochatechnologies.com>
2024-01-10 15:40:29 +0530

    seo content

--- src/components/Header/Head.js
+++ src/components/Header/Head.js
@@ -4,26 +4,26 @@ import { Helmet } from "react-helmet";
 const Head = () => {
   return (
     <Helmet>
-      <title>Landy - React Template</title>
+      <title>Oneadmin.in</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       <meta name="description"
-        content="Landy is a free React landing page template designed for developers and startups." />
+        content="Oneadmin is a free attendance management, fee management, course management Web application." />
       <meta name="keywords"
-        content="Landy is a free React landing page template designed for developers and startups." />
-      <meta property="og:title" content="Landy - React Template" />
+        content="Oneadmin is a free attendance management, fee management, course management Web application" />
+      <meta property="og:title" content="oneadmin|Education Management Simplified" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://landy.website/" />
+      <meta property="og:url" content="https://oneadmin.in" />
       <meta property="og:description"
-        content="Landy is a free React landing page template designed for developers and startups." />
+        content="Oneadmin is a free attendance management, fee management, course management Web application" />
       <meta name="robots" content="index, follow" />
       <meta property="og:locale" content="en_EN" />
-      <meta property="og:site_name" content="Landy" />
-      <meta property="og:url" content="https://landy.website/" />
-      <meta property="twitter:title" content="Landy - React Template" />
+      <meta property="og:site_name" content="Oneadmin" />
+      <meta property="og:url" content="https://oneadmin.in" />
+      <meta property="twitter:title" content="Oneadmin|Education Management Simplified" />
       <meta property="twitter:description"
-        content="Landy is a free React landing page template designed for developers and startups." />
-      <meta property="twitter:site" content="https://landy.website/" />
+        content="Oneadmin is a free attendance management, fee management, course management Web application" />
+      <meta property="twitter:site" content="https://oneadmin.in" />
       <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet" async />
     </Helmet>
   );
